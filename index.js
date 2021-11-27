dde54da22531f36f32ab7fcb430b9c4a00aa4b67
Kent Brockman <brockman.ks@gmail.com>
2021-11-26 21:04:33 -0700

    start putting stuff into functions

--- index.js
+++ index.js
@@ -1,19 +1,57 @@
 const https = require('https');
 const fs = require('fs');
 
-const API = "https://wiki.protospace.ca/api.php";
+function getPage(name, callback) {
+  const API = "https://wiki.protospace.ca/api.php";
+  // TODO: string builder? what is risk of injection attack?
+  let request = API + "?action=parse&prop=wikitext&format=json&page=" + name
+  console.log("requesting", request);
+  https.get(request, res => {
+    res.setEncoding("utf-8");
+    let body = "";
+    res.on("data", data => {
+      body += data;
+    });
 
-// request = API + "?action=parse&page=Laser_cutter,_large_(Rabbit_Laser_RL-80-1290)_ID:6&prop=text"
-request = API + "?action=parse&page=6&prop=text"
-https.get(request, res => {
-  res.setEncoding("utf-8");
-  let body = "";
-  res.on("data", data => {
-    body += data;
+    res.on("end", (err) => {
+      // check if this page has a redirect
+      // protospace wiki pages for tools often do
+      // TODO: exception handling
+      resp = JSON.parse(body).parse;
+      wikitext = resp.wikitext["*"];
+      if (isRedirect(wikitext)) {
+        redirect_page = extractRedirect(wikitext)
+        console.log("CALL REDIRECT", redirect_page);
+        getPage(redirect_page, callback);
+        return
+      }
+
+      // otherwise, pass the response data back
+      callback(resp, err);
+    })
   });
+}
 
-  res.on("end", (err) => {
-    if(err) throw err;
-    console.log(body);
-  })
+function isRedirect(wikitext) {
+  return wikitext.includes("{{id/after-redirect}}");
+}
+
+function extractRedirect(wikitext) {
+  // regexr.com/6acsk
+  // the first group match here is for the Page we redirect to
+  let regex = new RegExp(/REDIRECT \[\[(.*)\]\]\{\{id\/after-redirect\}\}/);
+
+  // TODO: what do in case of no match?
+
+  // get the name of the page we are redirecting to
+  // TODO: this is horrific, can we use a named group or something instead of blindly indexing?
+  // https://www.bennadel.com/blog/3508-playing-with-regexp-named-capture-groups-in-node-10.htm
+  return regex.exec(wikitext)[1]
+}
+
+// main
+getPage("6", (body, err) => {
+  if (err) throw err;
+  console.log(body);
 });
+
