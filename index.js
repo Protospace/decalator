cdff2138cd88fb6b239a84a85b077a52047e8458
Kent Brockman <brockman.ks@gmail.com>
2021-12-03 20:24:07 -0700

    generate multiple labels...

--- index.js
+++ index.js
@@ -4,32 +4,30 @@ const path = require('path');
 const svgmod = require('./svgmod');
 const wikimediaApi = require('./wikimediaApi');
 
-print = console.log
+print = console.log;
 
 // TODO: proper logging
 // TODO: add function docs
 
 // main
-wikimediaApi.getPage('6', (body, err) => {
-  if (err) throw err;
-  params = wikimediaApi.extractNameAndId(body.title);
-  wikijump2x1 = svgmod.templates.wikijump2x1
-  template = svgmod.openSVG(wikijump2x1.path);
+[
+  '120',
+  '144'
+].forEach(page => {
+  wikimediaApi.getPage(page, (body, err) => {
+    if (err) throw err;
+    params = wikimediaApi.extractNameAndId(body.title);
+    wikijump2x1 = svgmod.templates.wikijump2x1;
+    template = svgmod.openSVG(wikijump2x1.path);
 
-  // replace url
-  new_url = wikimediaApi.WIKI_ENDPOINT + params.id;
-  urlElement = svgmod.getElement(template, wikijump2x1.url);
-  svgmod.replaceText(urlElement, new_url);
+    // replace contents
+    new_url = wikimediaApi.WIKI_ENDPOINT + params.id;
+    svgmod.replaceText(svgmod.getElement(template, wikijump2x1.url), new_url);
+    svgmod.replaceQRCode(svgmod.getElement(template, wikijump2x1.qrcode), new_url);
+    svgmod.replaceBoxedText(template, wikijump2x1.name, params.name);
+    svgmod.replaceText(svgmod.getElement(template, wikijump2x1.id), params.id);
 
-  // replace qr code
-  svgmod.replaceQRCode(svgmod.getElement(template, wikijump2x1.qrcode), new_url);
-
-  // replace name
-  svgmod.replaceBoxedText(template, wikijump2x1.name, params.name);
-
-  // replace id
-  svgmod.replaceText(svgmod.getElement(template, wikijump2x1.id), params.id);
-
-  // save to file
-  svgmod.saveAsPng(template, path.join(`./output/${params.id}`))
-});
+    // save to file
+    svgmod.saveAsPng(template, path.join(`./output/${params.id}`));
+  });
+})
